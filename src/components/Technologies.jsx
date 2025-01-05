e30e282e12181105fe02797132aeba17a1914e37
Rachoudane <95233992+Rachoudane@users.noreply.github.com>
2025-01-05 10:29:24 +0100

    Add Framer Motion animations
    - Integrated Framer Motion for smooth transitions.
    - Animated all sections: Hero, About, Experience, Projects, Technologies, and Contact.

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,41 +4,87 @@ import { FaUnity } from "react-icons/fa";
 import { FaJava } from "react-icons/fa";
 import { RiReactjsFill } from "react-icons/ri";
 import { RiTailwindCssFill } from "react-icons/ri";
+import { motion } from "framer-motion";
 
+const iconVariants = (duration) => ({
+  initial: {y: -10},
+  animate: {
+    y: [10, -10],
+    transition : {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
+    }
+  }
+)
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h2 className="my-20 text-center text-4xl">
+        <motion.h2
+        whileInView={{opacity: 2, y: 0}}
+        initial={{opacity: 0, y:-100}}
+        transition={{duration: 1.5}}
+        className="my-20 text-center text-4xl">
             Technologies
-        </h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
+        </motion.h2>
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial={{opacity: 0, x: -100}}
+        transition={{duration: 1.5}}
+        className="flex flex-wrap items-center justify-center gap-4">
 
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
             <TbBrandCSharp className="text-7xl text-neutral-200"/>
-            </div>
+            </motion.div>
 
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 p-4">
             <SiDotnet className="text-7xl text-blue-800"/>
-            </div>
+            </motion.div>
 
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div 
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 p-4">
             <FaUnity className="text-7xl text-neutral-600"/>
-            </div>
+            </motion.div>
 
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 p-4">
             <FaJava className="text-7xl text-white"/>
-            </div>
+            </motion.div>
 
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div 
+            variants={iconVariants(6)}
+            initial="initial"
+            animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 p-4">
             <RiReactjsFill className="text-7xl text-cyan-400"/>
-            </div>
+            </motion.div>
 
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div 
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate" 
+            className="rounded-2xl border-4 border-neutral-800 p-4">
             <RiTailwindCssFill className="text-7xl text-blue-400"/>
-            </div>
+            </motion.div>
 
-        </div>
+        </motion.div>
     </div>
   )
 }
