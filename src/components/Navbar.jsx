2fb96d9820afc899f977d9ccafd08644e2466d29
Rachoudane <95233992+Rachoudane@users.noreply.github.com>
2025-04-05 20:32:29 +0200

    fixed redirection
    fixed redirection to the same page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-        <a href="https://www.marouanedev.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.marouanedev.com">
           <img src={logo} alt="logo" className="cursor-pointer" />
         </a>
         </div>
