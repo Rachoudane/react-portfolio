598c750377ee7427fa56c4ffb2713427063bdeb3
Rachoudane <95233992+Rachoudane@users.noreply.github.com>
2025-04-05 22:21:58 +0200

    Update HeroAbout.jsx

--- src/components/HeroAbout.jsx
+++ src/components/HeroAbout.jsx
@@ -20,7 +20,7 @@ const HeroAbout = () => {
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8, delay: 0.2 }}
-          className="w-full lg:w-4/5 lg:order-first" // Changed to w-2/5 (40% width)
+          className="w-full lg:w-2/5 lg:order-first" // Changed to w-2/5 (40% width)
         >
           <div className="flex justify-center lg:justify-end">
             <img 
