1dd499e18b2673ef81c62b8e599ec7a064dc6b8c
Rachoudane <95233992+Rachoudane@users.noreply.github.com>
2025-04-05 20:30:22 +0200

    logo redirection
    logo redirection to marouanedev.com when clicked on it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import logo from "../assets/MarouaneBelkhdirLogo.png";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="logo" />
+        <a href="https://www.marouanedev.com" target="_blank" rel="noopener noreferrer">
+          <img src={logo} alt="logo" className="cursor-pointer" />
+        </a>
         </div>
     </nav>
   );
