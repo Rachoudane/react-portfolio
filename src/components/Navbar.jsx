35ef226fb0148e38f87bcc521e2ab1b4ae264f7b
Rachoudane <95233992+Rachoudane@users.noreply.github.com>
2025-04-05 20:25:13 +0200

    title + favicon changed
    - updated title to be displayed instantly
    - updated favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,6 @@ const Navbar = () => {
         <div className="flex flex-shrink-0 items-center">
             <img src={logo} alt="logo" />
         </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <a href="https://www.linkedin.com/in/marouanebelkhdir/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-            {/* Wrap GitHub icon in anchor tag to redirect */}
-            <a href="https://github.com/Rachoudane" target="_blank" rel="noopener noreferrer">
-                <FaGithub />
-            </a>
-        </div>
     </nav>
   );
 };
