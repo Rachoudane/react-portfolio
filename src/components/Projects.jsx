fc583aa0f46ec7ea245f46559d42951bf415aaf7
Rachoudane <95233992+Rachoudane@users.noreply.github.com>
2025-04-06 17:44:18 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,12 @@
-import { PROJECTS } from "../constants"
-import { motion } from "framer-motion"
+import { TEXTS } from "../constants";
+import { motion } from "framer-motion";
 
 const Projects = ({ currentLanguage }) => {
+  const { PROJECTS } = TEXTS[currentLanguage || "en"];
+
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <motion.h2 
+      <motion.h2
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
@@ -17,10 +19,10 @@ const Projects = ({ currentLanguage }) => {
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
             
-            <motion.div 
+            <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }} 
+              transition={{ duration: 1 }}
               className="w-full lg:w-1/4"
             >
               <img
@@ -28,22 +30,18 @@ const Projects = ({ currentLanguage }) => {
                 width={150}
                 height={150}
                 alt={project.title}
-                className="mb-6 rounded" 
+                className="mb-6 rounded"
               />
             </motion.div>
 
-            <motion.div 
+            <motion.div
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: 100 }}
               transition={{ duration: 1 }}
               className="w-full max-w-xl lg:w-3/4"
             >
-              <h6 className="mn-2 font-semibold">
-                {project.title}
-              </h6>
-              <p className="mb-4 text-neutral-400">
-                {project.description}
-              </p>
+              <h6 className="mn-2 font-semibold">{project.title}</h6>
+              <p className="mb-4 text-neutral-400">{project.description}</p>
               {project.technologies.map((tech, index) => (
                 <span
                   key={index}
@@ -58,7 +56,7 @@ const Projects = ({ currentLanguage }) => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
